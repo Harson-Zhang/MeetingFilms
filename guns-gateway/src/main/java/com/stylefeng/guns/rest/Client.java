c1bd3c88cc7ab2d24a36b447c1672778c9b30922
Harson <zhxharson@qq.com>
2019-08-02 19:17:10 +0800

    已有用户模块的工程

---
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/Client.java
@@ -0,0 +1,11 @@
+package com.stylefeng.guns.rest;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.stylefeng.guns.api.user.UserAPI;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Client {
+
+
+}
